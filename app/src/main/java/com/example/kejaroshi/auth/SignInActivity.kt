8aa95b41649b2f79e972963617329b75a287fc3f
adhimrahman <adhimrahaman@gmail.com>
2024-10-13 02:32:43 +0800

    Add: Gradient

--- app/src/main/java/com/example/kejaroshi/auth/SignInActivity.kt
+++ app/src/main/java/com/example/kejaroshi/auth/SignInActivity.kt
@@ -1,7 +1,9 @@
 package com.example.kejaroshi.auth
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -10,6 +12,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
@@ -17,22 +21,33 @@ import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavHostController
+import androidx.navigation.NavController
 import androidx.compose.ui.text.AnnotatedString
 
 @Composable
-fun SignInActivity(navController: NavHostController) {
+fun SignInActivity(navController: NavController) {
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     val passwordVisible by remember { mutableStateOf(false) }
 
+    val gradientColors = listOf(
+        Color(0xFFFCE4EC),  // Light pink
+        Color(0xFFF3E5F5),  // Light purple
+        Color(0xFFE8EAF6)   // Light indigo
+    )
+
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
+            .background(
+                brush = Brush.verticalGradient(colors = gradientColors)
+            ),
         contentAlignment = Alignment.Center
     ) {
         Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
@@ -75,8 +90,9 @@ fun SignInActivity(navController: NavHostController) {
             // Tombol Sign In
             Button(
                 onClick = {
-                    navController.navigate("home_page") // Navigasi ke HomeScreen
+                    navController.navigate("home_page")
                 },
+                shape = RoundedCornerShape(5.dp),
                 modifier = Modifier.fillMaxWidth()
             ) {
                 Text(text = "Sign In")
@@ -89,7 +105,7 @@ fun SignInActivity(navController: NavHostController) {
                 text = AnnotatedString("Don't have an account? Sign Up"),
                 style = TextStyle(color = MaterialTheme.colorScheme.primary),
                 modifier = Modifier.clickable {
-                    navController.navigate("signup_page") // Navigasi ke SignUpActivity
+                    navController.navigate("signup_page")
                 }
             )
         }
